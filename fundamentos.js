2dd4599e3aaf357d002ca7753cb983839c8a318a
Marl0nico <150805769+Marl0nico@users.noreply.github.com>
2024-10-07 21:17:30 -0500

    Add files via upload
    Consumo de una API con fetch

---
+++ fundamentos.js
@@ -0,0 +1,18 @@
+//variable - contenedor de datos
+var people=45 //forma clásica, ya no se recomienda su uso
+//const es constante, algo fijo
+//tipos de datos simples
+let data=42 //number
+let data2=42.5 //float
+let data3="Juan" //string
+let data4=false //boolean
+let data5 //undefined
+let data6=null //null
+let data7='@' //char
+console.log(data); //imprime en consola
+//tipos de datos compuestos
+let data8=['Peter', "Lore", "Carmen"] //array
+let data9=person={ //objeto
+    name: "Juan",
+    city: "Quito"
+}
